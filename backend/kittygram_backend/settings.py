ae778157bf045180302fb3d6d3d2002bc7ebcd5b
Stanislav Barinov <152168070+hix9@users.noreply.github.com>
2024-08-20 10:37:11 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,5 +1,4 @@
 # flake8: noqa
-import ast
 import os
 from pathlib import Path
 
@@ -14,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG_MODE')
 
-ALLOWED_HOSTS = ast.literal_eval(os.getenv('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS')).split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
